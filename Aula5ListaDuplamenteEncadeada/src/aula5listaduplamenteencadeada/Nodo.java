255d36c141ce33932a88c5b010f39229a3e7b591
Rafael Moreno <rafaelmorenosax@gmail.com>
2022-04-07 10:39:02 -0300

    lista circular

--- Aula5ListaDuplamenteEncadeada/src/aula5listaduplamenteencadeada/Nodo.java
+++ Aula5ListaDuplamenteEncadeada/src/aula5listaduplamenteencadeada/Nodo.java
@@ -9,5 +9,33 @@ package aula5listaduplamenteencadeada;
  * @author rafae
  */
 public class Nodo {
+    private float dado;
+    private Nodo anterior;
+    private Nodo proximo;
+
+    public float getDado() {
+        return dado;
+    }
+
+    public void setDado(float dado) {
+        this.dado = dado;
+    }
+
+    public Nodo getAnterior() {
+        return anterior;
+    }
+
+    public void setAnterior(Nodo anterior) {
+        this.anterior = anterior;
+    }
+
+    public Nodo getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(Nodo proximo) {
+        this.proximo = proximo;
+    }
+    
     
 }
